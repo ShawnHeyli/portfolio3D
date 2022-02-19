fa7d7bca2c040b16aaf5c3eac3905bdfaa9d2cbd
Hilan Meyran <hilanmeyran@protonmail.com>
2022-02-19 10:38:03 +0100

    Rewamped core css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import './App.css';
+import Home from './pages/Home';
+import Works from './pages/Works';
+import Contact from './pages/Contact';
 
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header';
+import { AnimatePresence } from 'framer-motion';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
 
-export default function App() {
+const route = [
+	{ path: '/', name: 'Home', component: Home },
+	{ path: '/works', name: 'Works', component: Works },
+	{ path: '/about', name: 'Contact', component: Contact },
+];
+
+function App() {
+	const location = useLocation();
+
 	return (
-
+		<>
+			<Header />
+			<AnimatePresence exitBeforeEnter>
+				<Routes location={location} key={location.pathname}>
+					<Route path='/' element={<Home />} />
+					<Route path='/works' element={<Works />} />
+					<Route path='/contact' element={<Contact />} />
+				</Routes>
+			</AnimatePresence>
+		</>
 	);
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
