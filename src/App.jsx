633bd3d53deff6087b9ff97570cbaac73ff458cf
Hilan Meyran <hilanmeyran@protonmail.com>
2022-02-20 17:03:41 +0100

    App cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,6 @@ import Contact from './pages/Contact';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import { AnimatePresence } from 'framer-motion';
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
-
-const route = [
-	{ path: '/', name: 'Home', component: Home },
-	{ path: '/works', name: 'Works', component: Works },
-	{ path: '/about', name: 'Contact', component: Contact },
-];
 
 function App() {
 	const location = useLocation();
@@ -20,7 +12,7 @@ function App() {
 	return (
 		<>
 			<Header />
-			<AnimatePresence exitBeforeEnter>
+			<AnimatePresence initial={false} exitBeforeEnter>
 				<Routes location={location} key={location.pathname}>
 					<Route path='/' element={<Home />} />
 					<Route path='/works' element={<Works />} />
