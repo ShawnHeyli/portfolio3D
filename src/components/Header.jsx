c13f559bc164d1329209b4ce9c4060635979a3b8
Hilan Meyran <hilanmeyran@protonmail.com>
2022-02-26 12:30:26 +0100

    Header Rewamp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,38 +3,45 @@ import './styles/Header.scss';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function Header() {
+export default function Header(props) {
+	var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	const current = new Date();
+	const twoDigits = (time) => {
+		return `${time < 10 ? '0' + time : time}`;
+	};
+
+	const date = `
+  ${twoDigits(current.getHours())} : 
+  ${twoDigits(current.getMinutes())} - 
+  ${days[current.getDay()]}`;
+
+	const navLinkStyle = ({ isActive }) => {
+		return {
+			textDecoration: isActive ? `line-through 4px ${props.pageColor}` : '',
+			borderLeft: `solid 4px ${props.pageColor}`
+		};
+	};
+
 	return (
-		<header>
-			<NavLink
-				className='navLink'
-				to='/contact'
-				style={({ isActive }) =>
-					isActive ? { textDecoration: 'line-through 4px #fe8019' } : undefined
-				}
-			>
+		<header
+			style={{
+				borderBottom: `solid 4px ${props.pageColor}`,
+				borderTop: `solid 4px ${props.pageColor}`
+			}}
+		>
+			<p className='date'>
+				local information :<br />
+				{date}
+			</p>
+			<NavLink className='navLink' to='/contact' style={navLinkStyle}>
 				Contact
 			</NavLink>
-			<NavLink
-				className='navLink'
-				to='/'
-				style={({ isActive }) =>
-					isActive ? { textDecoration: 'line-through 4px #fe8019' } : undefined
-				}
-			>
+			<NavLink className='navLink' to='/' style={navLinkStyle}>
 				Hilan Meyran
 			</NavLink>
-			<NavLink
-				className='navLink'
-				to='/works'
-				style={({ isActive }) =>
-					isActive ? { textDecoration: 'line-through 4px #fe8019' } : undefined
-				}
-			>
+			<NavLink className='navLink' to='/works' style={navLinkStyle}>
 				Works
 			</NavLink>
 		</header>
 	);
 }
-
-export default Header;
