a7301a4c004c7999ebd2ce1fb67069db6ecc2111
Hilan Meyran <hilanmeyran@protonmail.com>
2022-02-26 12:31:22 +0100

    Home Rewamp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
-import { animate, motion } from 'framer-motion';
+import './styles/Home.scss';
+
+import Sidebar from '../components/Sidebar';
+
+import { motion } from 'framer-motion';
 import React from 'react';
 
-function Home() {
+function Home({ pageColor, backgroundImageUrl, floatSidebarUrl }) {
+	console.log(backgroundImageUrl);
 	return (
-		<motion.div
+		<motion.section
+			className='home'
 			initial={{ scaleY: 0 }}
 			animate={{ scaleY: 1 }}
 			exit={{ scaleY: 0 }}
 			transition={{ duration: 0.5 }}
 		>
-			Home
-		</motion.div>
+			<Sidebar pageColor={pageColor} floatSidebarUrl={floatSidebarUrl}>
+				<h3>Hilan Meyran, Portfolio</h3>
+				<p>
+					Hello, I’m Kazuki Noda, visual designer and art director specializing
+					in graphic design
+				</p>
+			</Sidebar>
+			<article
+				style={{ background: `url(${backgroundImageUrl}) right/cover` }}
+			></article>
+		</motion.section>
 	);
 }
 
