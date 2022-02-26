dcdb6eb700d4b8b0e11da8824cdf5988a54ca021
Hilan Meyran <hilanmeyran@protonmail.com>
2022-02-26 12:29:10 +0100

    moved content of index.scss to App.scss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
-			<App />
+			<AnimatePresence initial={false} exitBeforeEnter>
+				<App />
+			</AnimatePresence>
 		</Router>
 	</React.StrictMode>,
 	document.getElementById('root')
